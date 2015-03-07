76a7cc675ad6b5b4fde560227058f4cae96041f0
Bernd Vogt <bernd.vogt@sourcepit.org>
2015-03-07 21:19:54 +0100

    FormatSourceHandler

---
+++ org.sourcepit.antlr4.eclipse.ui/src/org/sourcepit/antlr4/eclipse/ui/FormatSourceHandler.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 Bernd Vogt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sourcepit.antlr4.eclipse.ui;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.sourcepit.antlr4.eclipse.lang.ANTLRv4Lexer;
+import org.sourcepit.antlr4.eclipse.lang.ANTLRv4Parser;
+import org.sourcepit.antlr4.eclipse.lang.symbols.ANTLRv4ScopeBuildingListener;
+
+/**
+ * @author Bernd Vogt <bernd.vogt@sourcepit.org>
+ */
+public class FormatSourceHandler extends AbstractHandler {
+
+   @Override
+   public Object execute(ExecutionEvent event) throws ExecutionException {
+
+      final ITextEditor activeEditor = (ITextEditor) HandlerUtil.getActiveEditorChecked(event);
+
+      final IDocument document = activeEditor.getDocumentProvider().getDocument(activeEditor.getEditorInput());
+
+      final ANTLRInputStream input;
+      try {
+         input = new ANTLRInputStream(new DocumentRangeReader(document, 0, document.getLength()));
+      }
+      catch (IOException e) {
+         throw new IllegalStateException(e);
+      }
+      final CommonTokenStream tokenStream = new CommonTokenStream(new ANTLRv4Lexer(input));
+      final ANTLRv4Parser parser = new ANTLRv4Parser(tokenStream);
+      final ANTLRv4ScopeBuildingListener scopeBuilder = new ANTLRv4ScopeBuildingListener();
+      parser.addParseListener(scopeBuilder);
+
+      parser.grammarSpec();
+
+      List<Token> tokens = tokenStream.getTokens();
+      for (Token token : tokens) {
+         System.out.print(token.getText());
+      }
+      return null;
+   }
+
+}
